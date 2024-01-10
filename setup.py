2d813d669f6af700dcf050ac5389908f8a7b14c0
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-01-10 21:57:19 +0000

    build(deps): update setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,12 @@ setup(
     version='1.3',
     author='ElliottSF',
     install_requires=[
-        'pandas',
-        'openpyxl'
-        'appdirs'
+        'tkcalendar==1.6.1',
+        'pandas==2.1.4',
+        'openpyxl==3.1.2',
+        'appdirs==1.4.4'
     ],
+    python_requires='==3.10.12',
     scripts=[
         'src/source.py',
         'src/utils.py'
