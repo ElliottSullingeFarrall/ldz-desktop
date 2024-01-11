853ae5f63d3b66e9933f57752f0f81907e014192
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-01-11 22:27:05 +0000

    update src/ldz/__init__.py

--- src/ldz/__init__.py
+++ src/ldz/__init__.py
@@ -28,7 +28,7 @@ class App(tk.Tk):
         self.style.theme_use('clam')
 
         self.title(f'Select Profile')
-        self.iconphoto(True, tk.PhotoImage(file=resource_path('images/stag.png')))
+        self.iconphoto(True, tk.PhotoImage(file='images/stag.png'))
         self.resizable(False, False)
         self.geometry('250x120')
 
