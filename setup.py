e439d368192763547dc6fca6304d8cf2a6cfdaac
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-01-11 23:45:18 +0000

    build(deps): update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open('requirements.txt') as requirements:
     install_requires = requirements.read().splitlines()
 
 setup(
-    install_requires=install_requires,
-    package_dir={'': 'src'},
-    package_data={'ldz': ['images/*']},
-    scripts = ['ldz/utils.py'],
+    install_requires = install_requires,
+    package_dir = {'': 'src'},
+    package_data = {'ldz': ['images/*']},
+    scripts = ['src/ldz/utils.py'],
     entry_points = {'console_scripts': ['ldz = ldz:main']}
 )
