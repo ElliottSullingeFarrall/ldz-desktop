027fc5a3272d908ba6024b6598d54e204ecc2cf4
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-01-11 14:00:42 +0000

    build(deps): update setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ setup(
         'src/source.py',
         'src/utils.py'
     ],
+    include_package_data=True,
+    package_data={'': ['src/images/*']},
     entry_points={
         # example: file some_module.py -> function main
         #'console_scripts': ['someprogram=some_module:main']
