e0adb2b5bb5993a0891cbc20242ad95d28b69804
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-01-11 16:51:26 +0000

    build(deps): update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
-with open('requirements.txt') as f:
-    install_requires = f.read().splitlines()
+with open('requirements.txt') as requirements:
+    install_requires = requirements.read().splitlines()
 
 setup(
-    install_requires=install_requires,
     package_dir={'': 'src'},
     package_data={'ldz': ['images/*']}
 )
