804e2e75dd26d5d3e434a25357fe97795d1d2e75
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-01-11 14:28:04 +0000

    build(deps): update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='ElliottSF',
     package_dir={"": "src"},
     package_data={'ldz': ['images/*']},
-    scripts=['source.py'],
+    scripts=['ldz/source.py'],
     entry_points={
         # example: file some_module.py -> function main
         #'console_scripts': ['someprogram=some_module:main']
