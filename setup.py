f610e7e8b5e8b2e6feb2aeefe8d86239b607dab2
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-01-11 22:08:16 +0000

    update 2 files and delete 5 files

--- setup.py
+++ setup.py
@@ -1,20 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('requirements.txt') as requirements:
     install_requires = requirements.read().splitlines()
 
 setup(
     install_requires=install_requires,
-    packages=[
-        'ldz'
-    ],
-    package_dir={
-        'ldz': 'src'
-    },
-    package_data={
-        'ldz': ['images/*']
-    },
-    entry_points = {
-        'console_scripts': ['ldz = src.__init__:main']
-    }
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    package_data={'ldz': ['images/*']},
+    entry_points = {'console_scripts': ['ldz = ldz:main']}
 )
