b08c3bb24062805be280a16052fa74d5e52a233b
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-03-08 14:12:35 +0000

    feat: create flakes.nix

---
+++ flakes.nix
@@ -0,0 +1,50 @@
+{
+  description = "A Nix flake for the LDZ application.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        poetry2nix = import poetry2nix { inherit pkgs; };
+      in
+      {
+        packages = {
+          ldz = poetry2nix.mkPoetryApplication rec {
+            pname = "ldz";
+            version = "v1.4";
+
+            projectDir = ./.;
+            python = pkgs.python3Full;
+            preferWheels = true;
+            # overrides = poetry2nix.defaultPoetryOverrides.extend (self: super:
+            #   builtins.mapAttrs (package: build-requirements:
+            #   (builtins.getAttr package super).overridePythonAttrs (old: {
+            #       buildInputs = (old.buildInputs or [ ]) ++ (builtins.map (pkg: if builtins.isString pkg then builtins.getAttr pkg super else pkg) build-requirements);
+            #   })
+            #   ) {};
+            desktopItem = pkgs.makeDesktopItem {
+              name = "LDZ";
+              comment = "App for use in the University of Surrey's LDZ";
+              desktopName = "LDZ App";
+              exec = "${pname}";
+              icon = "${pname}";
+              terminal = false;
+              type = "Application";
+            };
+            postPatch = ''
+              mkdir -p $out/share/applications
+              cp ${desktopItem}/share/applications/* $out/share/applications
+              mkdir -p $out/share/pixmaps
+              ln -s $out/lib/python3.11/site-packages/ldz/images/stag.png $out/share/pixmaps/ldz.png
+            '';
+          };
+        };
+      }
+    );
+}
