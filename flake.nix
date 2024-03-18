8634f1c169dc9b5f0a37b8078dd9e54fe2419fe3
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-03-18 15:01:03 +0000

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -21,6 +21,21 @@
         poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs;};
       in
       {
+        devShells = {
+          default = self.devShells.${system}.ldz;
+          ldz = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.ldz ];
+            packages = with pkgs; [ poetry python3Full ];
+
+            # LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+
+            POETRY_VIRTUALENVS_IN_PROJECT = true;
+            shellHook = ''
+              poetry env use $(which python)
+              poetry install --no-root
+            '';
+          };
+        };
         packages = {
           default = self.packages.${system}.ldz;
           ldz = poetry2nix.mkPoetryApplication rec {
@@ -30,12 +45,7 @@
             projectDir = ./.;
             python = pkgs.python3Full;
             preferWheels = true;
-            # overrides = poetry2nix.defaultPoetryOverrides.extend (self: super:
-            #   builtins.mapAttrs (package: build-requirements:
-            #   (builtins.getAttr package super).overridePythonAttrs (old: {
-            #       buildInputs = (old.buildInputs or [ ]) ++ (builtins.map (pkg: if builtins.isString pkg then builtins.getAttr pkg super else pkg) build-requirements);
-            #   })
-            #   ) {};
+
             desktopItem = pkgs.makeDesktopItem {
               name = "LDZ";
               comment = "App for use in the University of Surrey's LDZ";
@@ -50,21 +60,6 @@
               cp ${desktopItem}/share/applications/* $out/share/applications
               mkdir -p $out/share/pixmaps
               ln -s $out/lib/python3.11/site-packages/ldz/images/stag.png $out/share/pixmaps/ldz.png
-            '';
-          };
-        };
-        devShells = {
-          default = self.devShells.${system}.ldz;
-          ldz = pkgs.mkShell {
-            inputsFrom = [ self.packages.${system}.ldz ];
-            packages = with pkgs; [ poetry python3Full ];
-
-            # LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
-
-            POETRY_VIRTUALENVS_IN_PROJECT = true;
-            shellHook = ''
-              poetry env use $(which python)
-              poetry install --no-root
             '';
           };
         };
